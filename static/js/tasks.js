3a1aa3d122930b666fff2e1ddabb1f86cb13421c
garak505 <garak505@gmail.com>
2023-09-13 11:03:31 +0800

    refactor: apply coding style tweaks

--- static/js/tasks.js
+++ static/js/tasks.js
@@ -1,4 +1,3 @@
-
 /* function add_to_parent_task_queue(queue ,uuid) {
   if (getLocalStorage("parent_task_queue") != "") {
     queue = JSON.parse(getLocalStorage("parent_task_queue"));
@@ -16,6 +15,8 @@ function parent_task_queue(queue, uuid) {
   return queue;
 } */
 
+import { get_cropped_image } from "./utils/croppie";
+
 export function add_to_child_task_queue(uuid) {
   var queue = [];
   if (getLocalStorage("child_task_queue") != "") {
@@ -30,30 +31,30 @@ export function remove_child_task_queue(uuid) {
   var queue = [];
   if (getLocalStorage("child_task_queue") != "") {
     queue = JSON.parse(getLocalStorage("child_task_queue"));
-    queue = queue.filter(item => item !== uuid);
+    queue = queue.filter((item) => item !== uuid);
     setLocalStorage("child_task_queue", JSON.stringify(queue));
   }
   return queue;
 }
 
-export function deep_deleted_task(uuid){
+export function deep_deleted_task(uuid) {
   var dataJSON = {};
-  dataJSON.uuid=uuid
+  dataJSON.uuid = uuid;
 
   $.ajax({
-    url:HOST_URL_TPLANET_DAEMON + "/tasks/del_task",
+    url: HOST_URL_TPLANET_DAEMON + "/tasks/del_task",
     type: "POST",
     async: false,
     crossDomain: true,
-    data:  dataJSON,
-    success: function(returnData) {
+    data: dataJSON,
+    success: function (returnData) {
       const obj = JSON.parse(returnData);
-      dataJSON = obj
+      dataJSON = obj;
       // delete_div(obj)
     },
-    error: function(xhr, ajaxOptions, thrownError){
+    error: function (xhr, ajaxOptions, thrownError) {
       console.log(thrownError);
-    }
+    },
   });
 
   return dataJSON;
@@ -68,25 +69,24 @@ export function deep_deleted_task(uuid){
 //   }
 // }
 
-export function deleted_task(uuid){
+export function deleted_task(uuid) {
   var dataJSON = {};
-  dataJSON.uuid=uuid
+  dataJSON.uuid = uuid;
 
   $.ajax({
-    url:HOST_URL_TPLANET_DAEMON + "/tasks/del_task",
+    url: HOST_URL_TPLANET_DAEMON + "/tasks/del_task",
     type: "POST",
     async: false,
     crossDomain: true,
-    data:  dataJSON,
-    success: function(returnData) {
+    data: dataJSON,
+    success: function (returnData) {
       const obj = JSON.parse(returnData);
-      dataJSON = obj
-      location.reload()
-
+      dataJSON = obj;
+      location.reload();
     },
-    error: function(xhr, ajaxOptions, thrownError){
+    error: function (xhr, ajaxOptions, thrownError) {
       console.log(thrownError);
-    }
+    },
   });
 
   return dataJSON;
@@ -99,35 +99,35 @@ export function get_task_info(uuid) {
     type: "GET",
     async: false,
     crossDomain: true,
-    data:  dataJSON,
-    success: function(returnData) {
-       const obj = JSON.parse(returnData);
-       dataJSON = obj;
+    data: dataJSON,
+    success: function (returnData) {
+      const obj = JSON.parse(returnData);
+      dataJSON = obj;
     },
-    error: function(xhr, ajaxOptions, thrownError){
+    error: function (xhr, ajaxOptions, thrownError) {
       console.log(thrownError);
-    }
+    },
   });
   return dataJSON;
 }
 export function task_submit(form) {
   var resultJSON = {};
   $.ajax({
-    "url": HOST_URL_TPLANET_DAEMON + "/tasks/new",
-    "method": "POST",
-    "async": false,
-    "timeout": 0,
-    "processData": false,
-    "mimeType": "multipart/form-data",
-    "contentType": false,
-    "data": form,
-    success: function(returnData) {
-       const obj = JSON.parse(returnData);
-       resultJSON = obj;
+    url: HOST_URL_TPLANET_DAEMON + "/tasks/new",
+    method: "POST",
+    async: false,
+    timeout: 0,
+    processData: false,
+    mimeType: "multipart/form-data",
+    contentType: false,
+    data: form,
+    success: function (returnData) {
+      const obj = JSON.parse(returnData);
+      resultJSON = obj;
     },
-    error: function(xhr, ajaxOptions, thrownError){
+    error: function (xhr, ajaxOptions, thrownError) {
       console.log(thrownError);
-    }
+    },
   });
   return resultJSON;
 }
@@ -139,14 +139,14 @@ export function list_children_tasks(task) {
     type: "POST",
     async: false,
     crossDomain: true,
-    data:  dataJSON,
-    success: function(returnData) {
-       const obj = JSON.parse(returnData);
-       dataJSON = obj;
+    data: dataJSON,
+    success: function (returnData) {
+      const obj = JSON.parse(returnData);
+      dataJSON = obj;
     },
-    error: function(xhr, ajaxOptions, thrownError){
+    error: function (xhr, ajaxOptions, thrownError) {
       console.log(thrownError);
-    }
+    },
   });
   return dataJSON.task;
 }
@@ -164,14 +164,16 @@ export async function submitTaskCover(base64Img, uuid_task) {
       type: "POST",
       async: true,
       crossDomain: true,
-      data:  dataJSON,
+      data: dataJSON,
     };
-    $.ajax(settings).done(function (response) {
-      resultJSON = JSON.parse(response);
-      resolve(resultJSON); // 在响应解析后解析 Promise
-    }).fail(function (error) {
-      reject(error); // 在发生错误时拒绝 Promise
-    });
+    $.ajax(settings)
+      .done(function (response) {
+        resultJSON = JSON.parse(response);
+        resolve(resultJSON); // 在响应解析后解析 Promise
+      })
+      .fail(function (error) {
+        reject(error); // 在发生错误时拒绝 Promise
+      });
   });
 }
 
@@ -207,102 +209,123 @@ export async function uploadTaskCover(uuid_task) {
   // Get project uuid
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  var uuid = urlParams.get("uuid")
+  var uuid = urlParams.get("uuid");
 
   // Preview
   await upload_image_file(443, 300, "coverImg_" + uuid_task, true);
 
-  prepare_task_cover_upload().then(async function () {
-    var coverImg = document.getElementById( "coverImg_" + uuid_task).style.backgroundImage.replace('url("', '');
-    coverImg = coverImg.replace('")', '');
+  prepare_task_cover_upload()
+    .then(async function () {
+      var coverImg = document
+        .getElementById("coverImg_" + uuid_task)
+        .style.backgroundImage.replace('url("', "");
+      coverImg = coverImg.replace('")', "");
 
-    var resultJSON = submitTaskCover(coverImg, uuid_task);
+      var resultJSON = submitTaskCover(coverImg, uuid_task);
 
-    return resultJSON;
-  }).then(async function (resultJSON) {
-    if (resultJSON.result == "true") {
-      alert("更新成功");
-    } else {
-      alert("更新失敗，請洽系統管理員。");
-    }
-    stop_loading();
-  });
+      return resultJSON;
+    })
+    .then(async function (resultJSON) {
+      if (resultJSON.result == "true") {
+        alert("更新成功");
+      } else {
+        alert("更新失敗，請洽系統管理員。");
+      }
+      stop_loading();
+    });
 }
 
-export function child_task_submit(page){
+export function child_task_submit(page) {
   // Get DOM data for parent task
-    if (getLocalStorage("child_task_queue") == "")
-      return;
+  if (getLocalStorage("child_task_queue") == "") return;
 
-    // Get parent uuid
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    var uuid = urlParams.get("uuid")
-    var task = urlParams.get("task")
+  // Get parent uuid
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  var uuid = urlParams.get("uuid");
+  var task = urlParams.get("task");
 
-    var child_task_queue = JSON.parse(getLocalStorage("child_task_queue"));
+  var child_task_queue = JSON.parse(getLocalStorage("child_task_queue"));
 
-    for (var index_child_task = 0; index_child_task < child_task_queue.length; index_child_task++) {
-      var list_target_sdgs = [];
-      var list_tasks = [];
+  for (
+    var index_child_task = 0;
+    index_child_task < child_task_queue.length;
+    index_child_task++
+  ) {
+    var list_target_sdgs = [];
+    var list_tasks = [];
 
-      for (var index = 1; index < 18; index++) {
-        var index_sdg = ("0" + index).slice(-2);
-        if (document.getElementById("target_sdgs_" + index_sdg) == null) {
-          continue;
-        }
-        // Get SDGs tasks
-        var dataJSON = {};
-        dataJSON.sdg = index;
-        dataJSON.des = document.getElementById("target_sdgs_" + index_sdg).value;
-        list_tasks.push(JSON.stringify(dataJSON));
+    for (var index = 1; index < 18; index++) {
+      var index_sdg = ("0" + index).slice(-2);
+      if (document.getElementById("target_sdgs_" + index_sdg) == null) {
+        continue;
       }
-      // Set parent task
-      var dataJSONTask = {};
-      dataJSONTask.task_parent_id = task;
-      list_tasks.push(JSON.stringify(dataJSONTask));
-      setLocalStorage("list_target_sdgs", JSON.stringify(list_tasks));
-      // Task submit ...
-      var list_target_sdgs = getLocalStorage("list_target_sdgs");
-      var obj_list_target_sdgs = JSON.parse(list_target_sdgs);
+      // Get SDGs tasks
+      var dataJSON = {};
+      dataJSON.sdg = index;
+      dataJSON.des = document.getElementById("target_sdgs_" + index_sdg).value;
+      list_tasks.push(JSON.stringify(dataJSON));
+    }
+    // Set parent task
+    var dataJSONTask = {};
+    dataJSONTask.task_parent_id = task;
+    list_tasks.push(JSON.stringify(dataJSONTask));
+    setLocalStorage("list_target_sdgs", JSON.stringify(list_tasks));
+    // Task submit ...
+    var list_target_sdgs = getLocalStorage("list_target_sdgs");
+    var obj_list_target_sdgs = JSON.parse(list_target_sdgs);
 
-      /* if (obj_list_target_sdgs.length <= 1) {
+    /* if (obj_list_target_sdgs.length <= 1) {
         alert("您忘記新增權重了！");
         return false;
       } */
 
-      var form = new FormData();
-      // Add params cms_deep_participation.html
-      var type = 3;
-      form.append("task", child_task_queue[index_child_task]);
+    var form = new FormData();
+    // Add params cms_deep_participation.html
+    var type = 3;
+    form.append("task", child_task_queue[index_child_task]);
 
-      form.append("name", document.getElementById("name_" + child_task_queue[index_child_task]).value);
-      form.append("task_start_date", document.getElementById("task_start_date_" + child_task_queue[index_child_task]).value);
-      form.append("task_due_date", document.getElementById("task_due_date_" + child_task_queue[index_child_task]).value);
+    form.append(
+      "name",
+      document.getElementById("name_" + child_task_queue[index_child_task])
+        .value
+    );
+    form.append(
+      "task_start_date",
+      document.getElementById(
+        "task_start_date_" + child_task_queue[index_child_task]
+      ).value
+    );
+    form.append(
+      "task_due_date",
+      document.getElementById(
+        "task_due_date_" + child_task_queue[index_child_task]
+      ).value
+    );
 
-      // Task submit
-      form.append("uuid", uuid);
+    // Task submit
+    form.append("uuid", uuid);
 
-      // Add parent task ID
-      var list_tasks = [];
-      var pernet_task = {"task_parent_id": task}
-      list_tasks.push(JSON.stringify(pernet_task))
+    // Add parent task ID
+    var list_tasks = [];
+    var pernet_task = { task_parent_id: task };
+    list_tasks.push(JSON.stringify(pernet_task));
 
-      const container_id = "icon_container_" + child_task_queue[index_child_task];
-      const items = $(`#${container_id} [id^=target_sdgs_]`).toArray();
-      const task_data = items.map((item) => {
-        const sdg = parseInt($(item).attr('id').replace("target_sdgs_",""));
-        const data = {"sdg": sdg.toString(), "des": ""};
-        return JSON.stringify(data);
-      });
-      list_tasks = [...list_tasks, ...task_data];
+    const container_id = "icon_container_" + child_task_queue[index_child_task];
+    const items = $(`#${container_id} [id^=target_sdgs_]`).toArray();
+    const task_data = items.map((item) => {
+      const sdg = parseInt($(item).attr("id").replace("target_sdgs_", ""));
+      const data = { sdg: sdg.toString(), des: "" };
+      return JSON.stringify(data);
+    });
+    list_tasks = [...list_tasks, ...task_data];
 
-      form.append("tasks", list_tasks);
-      form.append("email", getLocalStorage("email"));
-      form.append("type", type);
-      var obj_result = task_submit(form);
-    }
-    setLocalStorage("child_task_queue", "");
+    form.append("tasks", list_tasks);
+    form.append("email", getLocalStorage("email"));
+    form.append("type", type);
+    var obj_result = task_submit(form);
+  }
+  setLocalStorage("child_task_queue", "");
   return true;
 }
 export function get_task_comment(uuid) {
@@ -313,14 +336,14 @@ export function get_task_comment(uuid) {
     type: "POST",
     async: false,
     crossDomain: true,
-    data:  dataJSON,
-    success: function(returnData) {
-       const obj = JSON.parse(returnData);
-       dataJSON = obj;
+    data: dataJSON,
+    success: function (returnData) {
+      const obj = JSON.parse(returnData);
+      dataJSON = obj;
     },
-    error: function(xhr, ajaxOptions, thrownError){
+    error: function (xhr, ajaxOptions, thrownError) {
       console.log(thrownError);
-    }
+    },
   });
   return dataJSON;
 }
@@ -333,14 +356,14 @@ export function verify_task_on_tplanet(uuid, listEmail) {
     type: "POST",
     async: false,
     crossDomain: true,
-    data:  dataJSON,
-    success: function(returnData) {
-       const obj = JSON.parse(returnData);
-       dataJSON = obj;
+    data: dataJSON,
+    success: function (returnData) {
+      const obj = JSON.parse(returnData);
+      dataJSON = obj;
     },
-    error: function(xhr, ajaxOptions, thrownError){
+    error: function (xhr, ajaxOptions, thrownError) {
       console.log(thrownError);
-    }
+    },
   });
   return dataJSON;
 }
@@ -356,13 +379,13 @@ export function getTaskWeight(task_UUID) {
     async: false,
     crossDomain: true,
     data: dataJSON,
-    success: function(returnData) {
-       const obj = JSON.parse(returnData);
-       TaskWeight = obj;
+    success: function (returnData) {
+      const obj = JSON.parse(returnData);
+      TaskWeight = obj;
     },
-    error: function(xhr, ajaxOptions, thrownError){
+    error: function (xhr, ajaxOptions, thrownError) {
       console.log(thrownError);
-    }
+    },
   });
 
   return TaskWeight;
