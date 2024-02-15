57714652967225f556d1a3eb77a8a2d25e81bf36
Peter Chen <peterchen2017125@gmail.com>
2024-02-15 17:44:31 +0800

    feat: add chart api

--- static/js/init.js
+++ static/js/init.js
@@ -56,30 +56,30 @@ function logout() {
     type: "POST",
     async: false,
     crossDomain: true,
-    data:  dataJSON,
-    success: function(returnData) {
-       const obj = JSON.parse(returnData);
-       // Clear local storage
-       localStorage.clear();
+    data: dataJSON,
+    success: function (returnData) {
+      const obj = JSON.parse(returnData);
+      // Clear local storage
+      localStorage.clear();
 
-       window.location.replace("/index.html");
+      window.location.replace("/index.html");
     },
-    error: function(xhr, ajaxOptions, thrownError){
+    error: function (xhr, ajaxOptions, thrownError) {
       console.log(thrownError);
     }
   });
 }
 
 function set_navbar_animation() {
-  $('.navbar').hover(function() {
+  $('.navbar').hover(function () {
     $('.navbar').removeClass('blur');
   });
 
   var lastScrollTop = 0;
-  $(window).scroll(function(){
+  $(window).scroll(function () {
     var scrollTop = $(this).scrollTop();
     // scroll down
-    if (scrollTop > lastScrollTop){
+    if (scrollTop > lastScrollTop) {
       $('.navbar').addClass('blur');
       lastScrollTop = scrollTop;
       return;
@@ -97,8 +97,8 @@ function navbar(group) {
   set_navbar_animation()
 
   // home logo href
-  if (group == "200" || group == "201" ) {
-      document.getElementById("index_logo").href =
+  if (group == "200" || group == "201") {
+    document.getElementById("index_logo").href =
       "/backend/admin_agent_dashboard.html";
 
     try {
@@ -107,24 +107,69 @@ function navbar(group) {
         child.style.display = "none";
       }
 
+      var obj_div = document.createElement("li");
+      obj_div.className = "nav-item dropdown";
+
       var obj_a = document.createElement("a");
-      obj_a.className = "nav-link fw-bold";
-      obj_a.href="javascript:void(0)"
-      obj_a.onclick= function(e){
-        logout()
-      }
+      obj_a.className = "nav-link dropdown-toggle";
+      obj_a.href = "#";
+      obj_a.setAttribute("role", "button");
+      obj_a.setAttribute("data-toggle", "dropdown");
+      obj_a.setAttribute("aria-haspopup", "true");
+      obj_a.setAttribute("aria-expanded", "false");
+      obj_a.innerHTML = "<img src='/static/imgs/user.svg' width='30' height='30' class='d-inline-block align-top' alt=''>";
+
+      var obj_dropdown_menu = document.createElement("div");
+      obj_dropdown_menu.className = "dropdown-menu";
+      obj_dropdown_menu.style = "left: -100px;";
+
+      var obj_dropdown_item_index = document.createElement("a");
+      obj_dropdown_item_index.className = "dropdown-item";
+      obj_dropdown_item_index.href = "admin_index.html";
+      obj_dropdown_item_index.innerHTML = "<img src='/static/imgs/index.svg' style='width: 25px; height: 25px;'> 首頁管理";
+
+      var obj_dropdown_item_sustainable = document.createElement("a");
+      obj_dropdown_item_sustainable.className = "dropdown-item";
+      obj_dropdown_item_sustainable.href = "cms_agent.html";
+      obj_dropdown_item_sustainable.innerHTML = "<img src='/static/imgs/cooperate.svg' style='width: 25px; height: 25px;'> 永續專區";
 
 
-      var obj_img = document.createElement("img");
-      obj_img.className = "align-text-top";
-      obj_img.src="/static/imgs/logout.svg";
-      obj_img.style="height:25px";
-      obj_a.innerText = "登出";
+      var obj_dropdown_item_AI = document.createElement("a");
+      obj_dropdown_item_AI.className = "dropdown-item";
+      obj_dropdown_item_AI.href = "cms_ai_gpt.html";
+      obj_dropdown_item_AI.innerHTML = "<img src='/static/imgs/ai.svg' style='width: 30px; height: 30px;'> AI Eval";
 
-      obj_a.append(obj_img);
+      var obj_dropdown_item_news = document.createElement("a");
+      obj_dropdown_item_news.className = "dropdown-item";
+      obj_dropdown_item_news.href = "cms_news_list.html";
+      obj_dropdown_item_news.innerHTML = "<img src='/static/imgs/news.svg' style='width: 25px; height: 25px;'> 最新消息";
 
-      obj_account_status.append(obj_a);
-    } catch(e) { console.log(e) }
+      var obj_dropdown_item_contact = document.createElement("a");
+      obj_dropdown_item_contact.className = "dropdown-item";
+      obj_dropdown_item_contact.href = "cms_contact_us.html";
+      obj_dropdown_item_contact.innerHTML = "<img src='/static/imgs/contact_us.svg' style='width: 25px; height: 25px;'> 聯繫我們";
+
+      var obj_dropdown_item_logout = document.createElement("a");
+      obj_dropdown_item_logout.className = "dropdown-item";
+      obj_dropdown_item_logout.href = "javascript:logout();"
+      obj_dropdown_item_logout.innerHTML = "<img src='/static/imgs/logout.svg' style='width: 25px; height: 25px;'> 登出";
+
+      var obj_dropdown_item_delete = document.createElement("a");
+      obj_dropdown_item_delete.className = "dropdown-item";
+      obj_dropdown_item_delete.href = "admin_agent_accountDelete.html";
+      obj_dropdown_item_delete.innerHTML = "<img src='/static/imgs/delete.svg' style='width: 25px; height: 25px;'> 刪除帳號";
+
+      obj_dropdown_menu.appendChild(obj_dropdown_item_index);
+      obj_dropdown_menu.appendChild(obj_dropdown_item_sustainable);
+      obj_dropdown_menu.appendChild(obj_dropdown_item_AI);
+      obj_dropdown_menu.appendChild(obj_dropdown_item_news);
+      obj_dropdown_menu.appendChild(obj_dropdown_item_contact);
+      obj_dropdown_menu.appendChild(obj_dropdown_item_logout);
+      obj_dropdown_menu.appendChild(obj_dropdown_item_delete);
+      obj_div.appendChild(obj_a);
+      obj_div.appendChild(obj_dropdown_menu);
+      obj_account_status.appendChild(obj_div);
+    } catch (e) { console.log(e) }
   }
 }
 
