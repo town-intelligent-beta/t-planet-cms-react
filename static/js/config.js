204c6cd083adc8e487a4a2fb64a1041fbf01dd3d
HuangJyunYu <yillkid@gmail.com>
2024-06-27 22:06:00 +0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,10 +1,10 @@
 // Host
-HOST_URL_EID_DAEMON = "https://eid-backend.townway.com.tw";
-HOST_URL_TPLANET_DAEMON = "https://tplanet-backend.townway.com.tw";
+HOST_URL_EID_DAEMON = "https://beta-eid-backend.townway.com.tw";
+HOST_URL_TPLANET_DAEMON = "https://beta-tplanet-backend.townway.com.tw";
 // HOST_URL_TPLANET_DAEMON = "https://tplanet-backend.4impact.cc";
 
 // EID
-HOST_URL_EID = "https://eid.townway.com.tw";
+HOST_URL_EID = "https://beta-eid.townway.com.tw";
 
 // Site hoster
 SITE_HOSTERS = ["forus999@gmail.com", "secondhome2023.1@gmail.com", "mickeypeng@tpwl.org"]
