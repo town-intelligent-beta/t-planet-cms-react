9282786c5c03e656db3dadbb76a649e937aa61d3
yillkid <yillkid@gmail.com>
2024-07-12 19:14:58 +0800

    un-tracking static folder

--- static/js/navbar.js
+++
@@ -1,7 +0,0 @@
-function set_page_navbar_active() {
-  // Get path
-  var path = window.location.pathname;
-  var page = path.split("/").pop();
-
-  // $("#nav-issues").addClass("active");
-}
--- static/js/utils/format.js
+++
@@ -1,9 +0,0 @@
-export const formatCurrency = (number, digits = 0) => {
-  const formatted =  new Intl.NumberFormat("zh-TW", {
-    style: "currency",
-    currency: "TWD",
-    minimumFractionDigits: digits,
-  }).format(number);
-
-  return formatted.replaceAll("$", "");
-};
