7117d19e5b563242cdbdb0152a3218bb8923b610
HuangJyunYu <yillkid@gmail.com>
2024-09-14 16:30:11 +0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -7,7 +7,7 @@ HOST_URL_LLMTWINS = "https://beta-llmtwins.4impact.cc"
 HOST_URL_EID = "https://beta-eid.townway.com.tw";
 
 // Site hoster
-SITE_HOSTERS = ["forus999@gmail.com", "secondhome2023.1@gmail.com", "mickeypeng@tpwl.org"]
+SITE_HOSTERS = ["forus999@gmail.com", "secondhome2023.1@gmail.com", "mickeypeng@tpwl.org", "400@gmail.com"]
 
 // Site information
 SITE_TYPE = 0 // 0 or 1(公板)
